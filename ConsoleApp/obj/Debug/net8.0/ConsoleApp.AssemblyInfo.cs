26d38ab30d06d5514a0542ea15018bace66a1cfa
Vladislav Sevtsenko <vlsevt@ttu.ee>
2024-10-25 02:18:54 +0300

    Removed unnecessary saves

--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d99d5a9192f554d6dc8e5e7271e98b64a6f49afd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
